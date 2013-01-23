5f01528b9e999a72eb2030f370415cd9a8543ca6
Vikki <vikki.read@gmail.com>
2013-01-23 00:25:47 +0000

    Added feedback form to every page using handlebar template, to avoid needing a pre-processor. Not in any way convinced this is the best way to do it. But I got to play with handlebars this way, and it was quick + easy. Also fairly rough code, but needed to hack something together :S Document that the feedback form posts to lives here : https://docs.google.com/a/unrulymedia.com/spreadsheet/viewform?formkey=dDlQMVpNV1kxT2JfYWxBLW5aYVgyNGc6MQ If you want access to the form results or to be on codeclubhalp@ then please let vikki know :)

---
+++ loadFeedback.js
@@ -0,0 +1,15 @@
+		(function getTemplateAjax(path) {
+	        var source;
+	        var template;
+
+	        $.ajax({
+	            url: path,
+	            cache: true,
+	            success: function(data) {
+	                source    = data;
+	                template  = Handlebars.compile(source);
+	                var tpl = $('<div id="target">').html(template({}));
+	                $('body').append(tpl);
+	            }               
+	        });         
+	    })('/templates/feedback_form.handlebars');
