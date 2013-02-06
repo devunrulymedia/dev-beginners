410bb72456d3dcdf3680e74d137ad9cd05e0523b
Vikki <vikki.read@gmail.com>
2013-02-06 12:47:53 +0000

    moar examples

---
+++ examples/ifs.js
@@ -0,0 +1,18 @@
+// use_html: ifs.html
+// Below is a definition of a drawing.
+
+function drawing() {
+  var myJob = "";
+  // add code to draw a person based on what myJob is
+  drawDelivery();
+}
+
+// The following functions are available:
+//
+// drawSell     - draw a sales/marketing person
+// drawDelivery - draw an delivery person (design, ops, media)
+// drawBuild    - draw a builder person (product, dev)
+// drawMoving   - draw a moving person (finance, p+p, exec)
+//
+// you can see all the pictures at http://www.unrulymedia.com/talent
+// ... or you can do the task and see em all that way :P
